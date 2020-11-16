eda6ce1215e5b5267d3bbda5647cd78fe203012f
John ODonnell <john.odonnell@cyberark.com>
2020-11-16 15:36:11 -0500

    Python client example PR comment fixes

--- examples/python_client.py
+++ examples/python_client.py
@@ -1,74 +1,68 @@
 #!/usr/bin/env python3
 
-import sys
-import time
 import base64
+import os
+import sys
+
 import openapi_client
 from openapi_client.rest import ApiException
 
-ACCOUNT_NAME = "cucumber"
+ACCOUNT_NAME = "dev"
+LOGIN = "admin"
+ADMIN_API_KEY = os.environ["CONJUR_ADMIN_API_KEY"]
 
-# setup API client config
+# Setup API client config
 config = openapi_client.Configuration()
-config.host = "http://localhost:3000"
-config.debug=True
-config.verify_ssl = False
-# config.assert_hostname = False
-config.username = "admin"
-config.password = "My-Passw0rd!"
+config.host = "http://conjur-https"
+# config.debug = True
+config.verify_ssl = True
+config.username = LOGIN
+config.password = ADMIN_API_KEY
 
-
-# new api client
 api_client = openapi_client.ApiClient(config)
-# authentication client
 login_api = openapi_client.AuthnApi(api_client)
 
-# login as admin, receiving admin API key in response
-api_key = None
-try:
-    api_key = login_api.login(account=ACCOUNT_NAME)
-    print("API key:", api_key)
-except ApiException as err:
-    print("Exception when logging in: ", err)
-    sys.exit(1)
-    
-# authenticate admin, receiving short-lived access token
+# Authenticate admin using basicAuth, receiving short-lived access token
+print("Authenticating admin...")
 access_token = None
 try:
-    access_token = login_api.authenticate(account=ACCOUNT_NAME, login="admin", body=api_key, accept_encoding="base64")
-    print(access_token)
+    access_token = login_api.authenticate(account=ACCOUNT_NAME, login=LOGIN, body=ADMIN_API_KEY, accept_encoding="base64")
+    print("Base64 encoded token:", access_token)
 except ApiException as err:
     print("Exception when authenticating in: ", err)
+    sys.exit(1)
 
-# change admin password using basicAuth
+# Change admin password, uses basicAuth
+print("\nChanging admin password...")
 new_password = "N3w-Passw0rd!"
 try:
     login_api.set_password(body=new_password, account=ACCOUNT_NAME)
     print("Password change successful.")
-    api_client.configuration.password = new_password
 except ApiException as err:
     print("Exception when setting password in: ", err)
     sys.exit(1)
+api_client.configuration.password = new_password
 
-# rotate admin API key
+# Rotate admin API key, uses basicAuth
+print("\nRotating admin API key...")
 try:
-    api_key = login_api.rotate_api_key(account=ACCOUNT_NAME)
-    print("New API key:", api_key)
+    ADMIN_API_KEY = login_api.rotate_api_key(account=ACCOUNT_NAME)
+    print("New API key:", ADMIN_API_KEY)
 except ApiException as err:
     print("Exception when logging in: ", err)
     sys.exit(1)
 
-# add Conjur Token header to client configuration
+# Add Conjur Token header to client configuration
 token_body = 'token="{}"'.format(access_token)
 api_client.configuration.api_key = {'conjurAuth': token_body}
 api_client.configuration.api_key_prefix = {'conjurAuth': 'Token'}
 
-# restore debug flag? stops printing request logs
-api_client.configuration.debug=True
-
-# load a policy using api client
 policy_api = openapi_client.PoliciesApi(api_client)
-policy="""- !variable
+
+# Load a policy using api client
+print("\nLoading root policy...")
+policy = """---
+- !variable
   id: sampleSecret
 
 - !permit
@@ -83,31 +77,33 @@ except ApiException as err:
     print("Exception when loading policy: ", err)
     sys.exit(1)
 
-# store a secret
+# Store a secret
+print("\nStoring secret...")
 secrets_api = openapi_client.SecretsApi(api_client)
 secret = "supersecretstuff"
 secret_id = "sampleSecret"
+print("Secret data: ", secret)
 try:
     secrets_api.create_variable(account=ACCOUNT_NAME, kind="variable", identifier=secret_id, body=secret)
-    print("Stored secret: ", secret)
+    print("Secret stored.")
 except ApiException as err:
     print("Exception when creating secret: ", err)
     sys.exit(1)
-print("Secret stored.")
 
-# retrieve secrets
-retrieved = None
+# Retrieve secrets
+print("\nRetrieving secret...")
+retrieved_secret = None
 try:
-    retrieved = secrets_api.get_variable(account=ACCOUNT_NAME, kind="variable", identifier=secret_id)
-    print("Retrieved seceret: ", retrieved)
+    retrieved_secret = secrets_api.get_variable(account=ACCOUNT_NAME, kind="variable", identifier=secret_id)
+    print("Retrieved seceret: ", retrieved_secret)
 except ApiException as err:
     print("Exception when retrieving secret: ", err)
     sys.exit(1)
-if retrieved == secret:
-    print("Secret Retrieved!")
-else:
+
+if retrieved_secret != secret:
     print("Secret Malformed.")
     print("Secret stored: ", secret)
     print("Secret retrieved: ", retrieved)
+    sys.exit(1)
 
-print("Done!")
+print("\nDone!")
