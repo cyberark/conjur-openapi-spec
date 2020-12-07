628b82a8543ac022c2f658488faebadfc77279ab
Ellis Wright <ellis.wright@cyberark.com>
2020-12-07 09:27:51 -0500

    Added tests for public keys api

---
+++ test/python/test_public_keys_api.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import unittest
+
+import openapi_client
+
+from . import api_config
+
+
+class TestPublicKeysApi(api_config.ConfiguredTest):
+    """PublicKeysApi unit test stubs"""
+
+    def setUp(self):
+        self.api = openapi_client.api.public_keys_api.PublicKeysApi(self.client)
+
+    def test_show_public_keys(self):
+        """Test case for show_public_keys
+
+        Shows all public keys for a resource.
+        """
+        resp = self.api.show_public_keys(self.account, 'Variable', 'one/password')
+
+        self.assertIsInstance(resp, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
