239b6c4508c84683da04455eae83195e1e3dc606
John ODonnell <john.odonnell@cyberark.com>
2020-12-28 15:29:45 -0500

    Add integration tests for /resources/{account}/{kind} endpoint
    Tests the full ranges of status code responses.
    Disables pylint reporting too many public classes in a single test class.

--- test/python/test_resources_api.py
+++ test/python/test_resources_api.py
@@ -42,86 +42,6 @@ class TestResourcesApi(api_config.ConfiguredTest):
 
         return openapi_client.ResourcesApi(alice_client)
 
-
-    # Test cases for /resources/{account}/{kind}/{identifier} endpoint
-
-    def test_get_resource_200(self):
-        """Test case for 200 status response on /resources/{account}/{kind}/{identifier}
-        200 - successful, role memberships returned as JSON
-        """
-        response = self.api.get_resource_with_http_info(
-            self.account,
-            'variable',
-            'testSecret'
-        )
-
-        self.assertEqual(response[1], 200)
-        self.assertIsInstance(response[0], dict)
-        for i in RESOURCE_MEMBERS:
-            self.assertIn(i, response[0])
-
-    def test_get_resource_204(self):
-        """Test case for 204 status response on /resources/{account}/{kind}/{identifier}
-        204 - permission check successful
-        """
-        response = self.api.get_resource_with_http_info(
-            self.account,
-            'variable',
-            'testSecret',
-            privilege="update",
-            check=True
-        )
-        self.assertEqual(response[1], 204)
-        self.assertEqual(response[0], "")
-
-    def test_get_resource_400(self):
-        """Test case for 400 status response on /resources/{account}/{kind}/{identifier} endpoint
-        400 - a required URI parameter is malformed
-        """
-        with self.assertRaises(openapi_client.ApiException) as context:
-            self.api.get_resource(self.account, NULL_BYTE, 'testSecret')
-
-        self.assertEqual(context.exception.status, 400)
-
-    def test_get_resource_401(self):
-        """Test case for 401 status response on /resources/{account}/{kind}/{identifier} endpoint
-        401 - unauthorized request
-        """
-        with self.assertRaises(openapi_client.ApiException) as context:
-            self.bad_auth_api.get_resource(self.account, 'variable', 'testSecret')
-
-        self.assertEqual(context.exception.status, 401)
-
-    def test_get_resource_404a(self):
-        """Test case for 404 status response on /resources/{account}/{kind}/{identifier} endpoint
-        404 - the requested resource does not exist
-        """
-        with self.assertRaises(openapi_client.ApiException) as context:
-            self.api.get_resource(self.account, 'variable', 'fakeVariable')
-
-        self.assertEqual(context.exception.status, 404)
-
-    def test_get_resource_404b(self):
-        """Test case for 404 status response on /resources/{account}/{kind}/{identifier} endpoint
-        404 - the authenticated user lacks the necessary privilege
-        Conjur docs say this should return 403 - Conjur api feature tests say otherwise
-        """
-        alice_resource_api = self.authenticated_client_without_privilege()
-
-        with self.assertRaises(openapi_client.ApiException) as context:
-            alice_resource_api.get_resource_with_http_info(self.account, 'variable', 'testSecret')
-
-        self.assertEqual(context.exception.status, 404)
-
-    def test_get_resource_422(self):
-        """Test case for 422 status response on /resources/{account}/{kind}/{identifier} endpoint
-        422 - an optional query parameter is malformed
-        """
-        with self.assertRaises(openapi_client.ApiException) as context:
-            self.api.get_resource(self.account, 'variable', 'testSecret', check=NULL_BYTE)
-
-        self.assertEqual(context.exception.status, 422)
-
     # Test cases for /resources endpoint
 
     def test_get_resources_200(self):
@@ -208,9 +128,144 @@ class TestResourcesApi(api_config.ConfiguredTest):
         """Test case for 422 status response on /resources/{account} endpoint
         422 - an optional query parameter is malformed
         """
-
         with self.assertRaises(openapi_client.ApiException) as context:
             self.api.get_resources_on_account(self.account, kind=NULL_BYTE)
+
+        self.assertEqual(context.exception.status, 422)
+
+    # Test cases for /resources/{account}/{kind} endpoint
+
+    def test_get_similar_resources_on_account_200(self):
+        """Test case for 200 status response on /resources/{account}/{kind}
+        200 - successful request, resources returned as JSON
+        """
+        response = self.api.get_similar_resources_on_account_with_http_info(
+            self.account,
+            "variable"
+        )
+
+        self.assertEqual(response[1], 200)
+        self.assertIsInstance(response[0], list)
+        for resource in response[0]:
+            for member in RESOURCE_MEMBERS:
+                self.assertIn(member, resource)
+
+    def test_get_similar_resources_on_account_400(self):
+        """Test case for 400 status response on /resources/{account}/{kind} endpoint
+        400 - a required URI parameter is malformed
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_similar_resources_on_account(NULL_BYTE, "variable")
+
+        self.assertEqual(context.exception.status, 400)
+
+    def test_get_similar_resources_on_account_401(self):
+        """Test case for 401 status response on /resources/{account}/{kind} endpoint
+        401 - unauthorized request
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.bad_auth_api.get_similar_resources_on_account(self.account, "variable")
+
+        self.assertEqual(context.exception.status, 401)
+
+    def test_get_similar_resources_on_account_403(self):
+        """Test case for 403 status response on /resources/{account}/{kind} endpoint
+        403 - the authenticated user lacks the necessary privilege
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_similar_resources_on_account(
+                self.account,
+                "variable",
+                acting_as="user:alice"
+            )
+
+        self.assertEqual(context.exception.status, 403)
+
+    def test_get_similar_resources_on_account_422(self):
+        """Test case for 422 status response on /resources/{account}/{kind}
+        422 - on optional query parameter is malformed
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_similar_resources_on_account(self.account, "variable", search=NULL_BYTE)
+
+        self.assertEqual(context.exception.status, 422)
+
+    # Test cases for /resources/{account}/{kind}/{identifier} endpoint
+
+    def test_get_resource_200(self):
+        """Test case for 200 status response on /resources/{account}/{kind}/{identifier}
+        200 - successful, role memberships returned as JSON
+        """
+        response = self.api.get_resource_with_http_info(
+            self.account,
+            'variable',
+            'testSecret'
+        )
+
+        self.assertEqual(response[1], 200)
+        self.assertIsInstance(response[0], dict)
+        for i in RESOURCE_MEMBERS:
+            self.assertIn(i, response[0])
+
+    def test_get_resource_204(self):
+        """Test case for 204 status response on /resources/{account}/{kind}/{identifier}
+        204 - permission check successful
+        """
+        response = self.api.get_resource_with_http_info(
+            self.account,
+            'variable',
+            'testSecret',
+            privilege="update",
+            check=True
+        )
+        self.assertEqual(response[1], 204)
+        self.assertEqual(response[0], "")
+
+    def test_get_resource_400(self):
+        """Test case for 400 status response on /resources/{account}/{kind}/{identifier} endpoint
+        400 - a required URI parameter is malformed
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_resource(self.account, NULL_BYTE, 'testSecret')
+
+        self.assertEqual(context.exception.status, 400)
+
+    def test_get_resource_401(self):
+        """Test case for 401 status response on /resources/{account}/{kind}/{identifier} endpoint
+        401 - unauthorized request
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.bad_auth_api.get_resource(self.account, 'variable', 'testSecret')
+
+        self.assertEqual(context.exception.status, 401)
+
+    def test_get_resource_404a(self):
+        """Test case for 404 status response on /resources/{account}/{kind}/{identifier} endpoint
+        404 - the requested resource does not exist
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_resource(self.account, 'variable', 'fakeVariable')
+
+        self.assertEqual(context.exception.status, 404)
+
+    def test_get_resource_404b(self):
+        """Test case for 404 status response on /resources/{account}/{kind}/{identifier} endpoint
+        404 - the authenticated user lacks the necessary privilege
+        Conjur docs say this should return 403 - Conjur api feature tests say otherwise
+        """
+        alice_resource_api = self.authenticated_client_without_privilege()
+
+        with self.assertRaises(openapi_client.ApiException) as context:
+            alice_resource_api.get_resource_with_http_info(self.account, 'variable', 'testSecret')
+
+        self.assertEqual(context.exception.status, 404)
+
+    def test_get_resource_422(self):
+        """Test case for 422 status response on /resources/{account}/{kind}/{identifier} endpoint
+        422 - an optional query parameter is malformed
+        """
+        with self.assertRaises(openapi_client.ApiException) as context:
+            self.api.get_resource(self.account, 'variable', 'testSecret', check=NULL_BYTE)
 
         self.assertEqual(context.exception.status, 422)
 
