078f03e2933bdec395b0de456e31b0a1f93410ce
Ellis Wright <ellis.wright@cyberark.com>
2021-01-11 16:38:53 -0500

    Expanded tests for policies spec endpoints
    Removed 500 responses from file to match expected api behavior

--- test/python/test_policies_api.py
+++ test/python/test_policies_api.py
@@ -25,45 +25,147 @@ UPDATE_POLICY = f'''
 
 class TestPoliciesApi(api_config.ConfiguredTest):
     """PoliciesApi unit test stubs"""
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        alice_client = api_config.get_api_client(username='alice')
+        cls.alice_api = openapi_client.api.policies_api.PoliciesApi(alice_client)
+
     def setUp(self):
         self.api = openapi_client.api.policies_api.PoliciesApi(self.client)
+        self.bad_auth_api = openapi_client.api.policies_api.PoliciesApi(self.bad_auth_client)
 
-    def test_load_policy(self):
-        """Test case for load_policy
+    def test_load_policy_201(self):
+        """Test case for load_policy 201 response
 
         Loads or replaces a Conjur policy document.
         """
-        self.api.load_policy(self.account, 'root', TEST_POLICY)
+        _, status, _ = self.api.load_policy_with_http_info(self.account, 'root', TEST_POLICY)
 
-        # Sanity check, make sure the new variable is accessable
-        secrets = openapi_client.api.secrets_api.SecretsApi(self.client)
-        secrets.create_variable(self.account, 'variable', NEW_VARIABLE, body='random_data')
+        self.assertEqual(status, 201)
 
-    def test_modify_policy(self):
-        """Test case for modify_policy
+    def test_load_policy_401(self):
+        """Test case for load_policy 401 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.bad_auth_api.load_policy(self.account, 'root', TEST_POLICY)
+
+        self.assertEqual(context.exception.status, 401)
+
+    def test_load_policy_400(self):
+        """Test case for load_policy 400 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.load_policy(self.account, '\00', TEST_POLICY)
+
+        self.assertEqual(context.exception.status, 400)
+
+    def test_load_policy_403(self):
+        """Test case for load_policy 403 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.alice_api.load_policy(self.account, 'root', TEST_POLICY)
+
+        self.assertEqual(context.exception.status, 403)
+
+    def test_load_policy_404(self):
+        """Test case for load_policy 404 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.load_policy(self.account, 'nonexist', TEST_POLICY)
+
+        self.assertEqual(context.exception.status, 404)
+
+    def test_load_policy_422(self):
+        """Test case for load_policy 422 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.load_policy(self.account, 'root', '\00')
+
+        self.assertEqual(context.exception.status, 422)
+
+    def test_modify_policy_201(self):
+        """Test case for modify_policy 201 response
 
         Modifies an existing Conjur policy.
         """
-        self.api.modify_policy(self.account, 'root', MODIFY_POLICY)
+        _, status, _ = self.api.modify_policy_with_http_info(self.account, 'root', MODIFY_POLICY)
 
-        # make sure the delete went through
-        secrets = openapi_client.api.secrets_api.SecretsApi(self.client)
+        self.assertEqual(status, 201)
 
-        with self.assertRaises(openapi_client.exceptions.ApiException):
-            secrets.get_variable(self.account, 'variable', NEW_VARIABLE)
+    def test_modify_policy_400(self):
+        """Test case for modify_policy 400 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.modify_policy(self.account, '\00', MODIFY_POLICY)
 
-    def test_update_policy(self):
-        """Test case for update_policy
+        self.assertEqual(context.exception.status, 400)
+
+    def test_modify_policy_401(self):
+        """Test case for modify_policy 401 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.bad_auth_api.modify_policy(self.account, 'root', MODIFY_POLICY)
+
+        self.assertEqual(context.exception.status, 401)
+
+    def test_modify_policy_403(self):
+        """Test case for modify_policy 403 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.alice_api.modify_policy(self.account, 'root', MODIFY_POLICY)
+
+        self.assertEqual(context.exception.status, 403)
+
+    def test_modify_policy_404(self):
+        """Test case for modify_policy 404 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.modify_policy(self.account, 'nonexist', MODIFY_POLICY)
+
+        self.assertEqual(context.exception.status, 404)
+
+    def test_modify_policy_422(self):
+        """Test case for modify_policy 422 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.modify_policy(self.account, 'root', '\00')
+
+        self.assertEqual(context.exception.status, 422)
+
+    def test_update_policy_201(self):
+        """Test case for update_policy 201 response
 
         Adds data to the existing Conjur policy.
         """
-        self.api.update_policy(self.account, 'root', UPDATE_POLICY)
+        _, status, _ = self.api.update_policy_with_http_info(self.account, 'root', UPDATE_POLICY)
 
-        # Test that we can set the new secret
-        secrets = openapi_client.api.secrets_api.SecretsApi(self.client)
-        secret_val = 'new secret value'
+        self.assertEqual(status, 201)
 
-        secrets.create_variable(self.account, 'variable', NEW_VARIABLE, body=secret_val)
+    def test_update_policy_400(self):
+        """Test case for update_policy 400 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.update_policy(self.account, '\00', UPDATE_POLICY)
+
+        self.assertEqual(context.exception.status, 400)
+
+    def test_update_policy_401(self):
+        """Test case for update_policy 401 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.bad_auth_api.update_policy(self.account, 'root', UPDATE_POLICY)
+
+        self.assertEqual(context.exception.status, 401)
+
+    def test_update_policy_403(self):
+        """Test case for update_policy 403 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.alice_api.update_policy(self.account, 'root', UPDATE_POLICY)
+
+        self.assertEqual(context.exception.status, 403)
+
+    def test_update_policy_404(self):
+        """Test case for update_policy 404 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.update_policy(self.account, 'nonexist', UPDATE_POLICY)
+
+        self.assertEqual(context.exception.status, 404)
+
+    def test_update_policy_422(self):
+        """Test case for update_policy 422 response"""
+        with self.assertRaises(openapi_client.exceptions.ApiException) as context:
+            self.api.update_policy(self.account, 'root', '\00')
+
+        self.assertEqual(context.exception.status, 422)
 
 if __name__ == '__main__':
     unittest.main()
