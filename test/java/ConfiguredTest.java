a4d7b439b1f123458618f22bb0837edd2441cc6f
telday <ellis.wright@cyberark.com>
2021-04-19 16:20:36 -0400

    Added Status API integration tests for Java client

--- test/java/ConfiguredTest.java
+++ test/java/ConfiguredTest.java
@@ -2,10 +2,12 @@ package org.conjur.sdk.api;
 
 import org.conjur.sdk.*;
 import org.conjur.sdk.auth.*;
-import org.junit.Before;
-import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class ConfiguredTest {
     protected ApiClient client;
@@ -29,6 +31,36 @@ public class ConfiguredTest {
         basicAuth.setPassword(apiKey);
         
         setupAccessToken(apiKey);
+    }
+
+    private static final String OIDC_POLICY_FILE = "/config/oidc-webservice.yml";
+    public void setupOIDCWebservice() throws ApiException {
+        PoliciesApi policiesApi = new PoliciesApi();
+        SecretsApi secretsApi = new SecretsApi();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(OIDC_POLICY_FILE));
+            String policyText = String.join(System.lineSeparator(), lines);
+            policiesApi.updatePolicy(account, "root", policyText, null);
+            secretsApi.createSecret(
+                account,
+                "variable",
+                "conjur/authn-oidc/test/provider-uri",
+                null,
+                null,
+                "https://keycloak:8443/auth/realms/master"
+            );
+
+            secretsApi.createSecret(
+                account,
+                "variable",
+                "conjur/authn-oidc/test/id-token-user-property",
+                null,
+                null,
+                "preferred_username"
+            );
+        } catch (IOException e) {
+            Assert.fail("Failed to read OIDC webservice policy file");
+        }
     }
 
     @Before
